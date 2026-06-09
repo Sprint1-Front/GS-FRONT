0bdab2aa9d3e5c6fbb53a297f86a082ab03672b1
DiedrichPaulo <paulominezada@gmail.com.br>
2026-06-09 18:30:30 -0300

    feat: criando o componente e imprtando dependências.

---
+++ gs-front/src/routes/Dashboard/Foco/FocoNovo.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import type { tipoFocoPoluicao } from "../../../types/tipoFocoPoluicao";
+
+const API_URL_FOCOS = "https://thalassor.onrender.com/focos";
+
+export default function FocoNovo() {
+const navigate = useNavigate();
+return(
+    <></>
+);
+}
