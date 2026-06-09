9170b00da9af10986dd3af4b8b50b51fabe863c9
DiedrichPaulo <paulominezada@gmail.com.br>
2026-06-09 18:36:36 -0300

    feat: adicionando estados ao formulário.

--- gs-front/src/routes/Dashboard/Foco/FocoNovo.tsx
+++ gs-front/src/routes/Dashboard/Foco/FocoNovo.tsx
@@ -6,6 +6,18 @@ const API_URL_FOCOS = "https://thalassor.onrender.com/focos";
 
 export default function FocoNovo() {
 const navigate = useNavigate();
+const [formData, setFormData] = useState<Partial<tipoFocoPoluicao>>({
+    idRegiao: 0,
+    idUsuario: 0,
+    latitude: 0,
+    longitude: 0,
+    extensaoKm2: 0,
+    indiceFdi: 0,
+    urlImagem: "",
+    statusFoco: "Ativo",
+    dataDeteccao: new Date().toISOString().split("T")[0], 
+    nivelRisco: "Baixo", 
+});
 return(
     <></>
 );
