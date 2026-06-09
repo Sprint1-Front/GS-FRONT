e59a3ba3d8bc40a1abb9c2a6e39dc5bfc1325fb3
guilheroliverfeitosa <guilhermeoliveirafeitosafacul@gmail.com>
2026-06-09 10:06:13 -0300

    feat: adicionando constantes da URL da API.

--- gs-front/src/routes/Dashboard/index.tsx
+++ gs-front/src/routes/Dashboard/index.tsx
@@ -1,5 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Dashboard() {
+  const navigate = useNavigate();
+
+  const API_URL_FOCOS = "https://thalassor.onrender.com/focos"; // URL do backend para obter os focos de incêndio
+  const API_URL_PREVISOES = "https://thalassor.onrender.com/previsoes"; // URL do backend para obter as previsões
+  const API_URL_SENSORES = "https://thalassor.onrender.com/sensores"; // URL do backend para obter os dados dos sensores
+  const API_URL_REGIOES = "https://thalassor.onrender.com/regioes"; // URL do backend para obter as regiões
+
   return (
-    <div className="">index</div>
-  )
+  <div>Dashboard</div>
+  );
 }
